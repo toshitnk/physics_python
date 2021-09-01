30dfdf499cdfee6444b40b20f2170fd1204fa3d6
{toshitnk} <{toshiya0417tanaka@gmail.com}>
2021-09-01 19:19:22 +0900

    first commit

---
+++ statmech/blackbody.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.rcParams['font.family'] = 'Arial'
+plt.rcParams['mathtext.fontset'] ='cm'
+plt.rcParams['xtick.labelsize'] = 15
+plt.rcParams['ytick.labelsize'] = 15
+plt.rcParams['xtick.direction'] = 'in'
+plt.rcParams['ytick.direction'] = 'in'
+plt.rcParams['xtick.major.size'] = 12
+plt.rcParams['ytick.major.size'] = 12
+plt.rcParams['xtick.major.pad'] = 18
+plt.rcParams['ytick.major.pad'] = 18
+plt.rcParams['axes.linewidth'] = 1.0
+plt.rcParams['figure.subplot.bottom'] = 0.2
+plt.rcParams['figure.subplot.left'] = 0.2
